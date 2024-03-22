d5487fdb1c8f422bd58c46842b0520ab8f217bc8
Anthony Giusti Shindelus <anthonyshindelus@gmail.com>
2024-03-22 13:27:12 -0700

    Beginning class for grid manager. Set project to open to dungeon level for editor default.

---
+++ Source/DungeonCrawler2024/GridManager.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GridManager.h"
+
+// Sets default values and other construction things
+AGridManager::AGridManager()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+}
+
+// Called when the game starts or when spawned
+void AGridManager::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGridManager::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/DungeonCrawler2024/GridManager.h
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "GridManager.generated.h"
+
+UCLASS()
+class DUNGEONCRAWLER2024_API AGridManager : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AGridManager();
+
+	UPROPERTY(EditAnywhere)
+	int GridWidth;
+	UPROPERTY(EditAnywhere)
+	int GridHeight;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
